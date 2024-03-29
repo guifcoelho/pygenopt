5f0aa15175297b642c38bc9ac77df8b816195f09
Guilherme Coelho <coelhoxz@gmail.com>
2024-03-29 17:56:31 -0300

    renaming and improving

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,23 +1,27 @@
 import pygenopt as opt
 from pygenopt.highsapi import HiGHS
 
-
-if __name__ == '__main__':
+def main():
     x1 = opt.Variable('x1')
     x2 = opt.Variable('x2')
 
     constr1 = opt.LinearConstraint(x2 - x1 >= 2, 'constr1')
     constr2 = opt.LinearConstraint(x1 + x2 >= 0, 'constr2')
 
+    objective_function = x1 + x2 + 5
+
     m = (
         opt.Model()
         .add_vars(x1, x2)
         .add_constrs(constr1, constr2)
-        .set_objective(x1 + x2)
+        .set_objective(objective_function, is_minimization=True)
         .set_options({'presolve': 'off'})
         .set_solver(HiGHS)
         .run()
-        .del_var(x2)
-        .run()
         .fetch_solution()
     )
+
+    print(x1.value, x2.value)
+
+if __name__ == '__main__':
+    main()
