10c52eea619a091f3af4268efb60473931482fec
Guilherme Coelho <coelhoxz@gmail.com>
2024-04-26 10:16:23 -0300

    including dot operation

--- pygenopt/__init__.py
+++ pygenopt/__init__.py
@@ -1,4 +1,4 @@
 from pygenopt.mainstructures import Variable, LinearExpression, LinearConstraint, ObjectiveFunction
 from pygenopt.enums import VarType, SolveStatus
 from pygenopt.problem import Problem
-from pygenopt.utils import sum
+from pygenopt.utils import sum, dot
--- pygenopt/utils.py
+++ pygenopt/utils.py
@@ -6,3 +6,6 @@ def sum(values):
     if isinstance(values, dict):
         values = list(values.values())
     return LinearExpression() + _pysum(values)
+
+def dot(coefs, variables):
+    return sum(coef * var for coef, var in zip(coefs, variables))
