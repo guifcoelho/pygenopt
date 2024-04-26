db106987a4c01a69ed4edb57a9922b155bd988c1
Guilherme Coelho <coelhoxz@gmail.com>
2024-04-26 11:38:19 -0300

    raising type error

--- pygenopt/utils.py
+++ pygenopt/utils.py
@@ -1,6 +1,17 @@
 from pygenopt import LinearExpression
 
 
+def isiterable(obj):
+    try:
+        obj = iter(obj)
+    except TypeError:
+        return False
+    else:
+        return True
+
+def isnumeric(obj):
+    return isinstance(obj, (float, int))
+
 _pysum = sum
 def Sum(values):
     if isinstance(values, dict):
@@ -8,21 +19,19 @@ def Sum(values):
     return LinearExpression() + _pysum(values)
 
 def Dot(values1, values2):
-    def isiterator(obj):
-        try:
-            obj = iter(obj)
-        except TypeError:
-            return False
-        else:
-            return True
+    if (
+        not isiterable(values1) and not isnumeric(values1)
+        and isiterable(values2) and not isnumeric(values2)
+    ):
+        raise TypeError("Arguments must be numeric or iterables.")
 
     if isinstance(values1, (int, float)) and isinstance(values2, (int, float)):
         return values1 * values2
 
-    if isiterator(values1) and not isiterator(values2):
+    if isiterable(values1) and not isiterable(values2):
         return Dot(values1, [values2]*len(values1))
 
-    if not isiterator(values1) and isiterator(values2):
+    if not isiterable(values1) and isiterable(values2):
         return Dot([values1]*len(values2), values2)
 
     return Sum(va1 * va2 for va1, va2 in zip(values1, values2))
