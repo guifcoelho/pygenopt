93bccbb08f266062089e33e2bdff06a3aa25a64c
Guilherme Coelho <coelhoxz@gmail.com>
2024-04-26 11:35:05 -0300

    Adding some corner cases to Dot function

--- pygenopt/utils.py
+++ pygenopt/utils.py
@@ -7,5 +7,22 @@ def Sum(values):
         values = list(values.values())
     return LinearExpression() + _pysum(values)
 
-def Dot(coefs, variables):
-    return Sum(coef * var for coef, var in zip(coefs, variables))
+def Dot(values1, values2):
+    def isiterator(obj):
+        try:
+            obj = iter(obj)
+        except TypeError:
+            return False
+        else:
+            return True
+
+    if isinstance(values1, (int, float)) and isinstance(values2, (int, float)):
+        return values1 * values2
+
+    if isiterator(values1) and not isiterator(values2):
+        return Dot(values1, [values2]*len(values1))
+
+    if not isiterator(values1) and isiterator(values2):
+        return Dot([values1]*len(values2), values2)
+
+    return Sum(va1 * va2 for va1, va2 in zip(values1, values2))
